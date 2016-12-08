27490c50baf7a374f0782bfcc8988fbccba3ad8f
MickaelSURREL <mickael@alpixel.fr>
2016-12-08 15:09:36 +0100

    Init commit

---
+++ Gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var shell = require('gulp-shell');
+var cssnano = require('gulp-cssnano');
+var watch = require('gulp-watch');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var gutil = require('gulp-util');
+var rewriteCSS = require('gulp-rewrite-css');
+var autoprefixer = require('gulp-autoprefixer');
+
+
+
+gulp.task('front', function() {
+    gulp.src('css/front.less')
+        .pipe(less())
+        .pipe(cssnano({
+            'postcss-minify-font-values': true
+        }))
+        .pipe(autoprefixer({
+            browsers:"> 1%, last 2 versions, Safari >= 8"
+        }))
+        .pipe(rename({basename: 'front', suffix: '.min'}))
+        .pipe(gulp.dest('css/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('css/**/*.less', ['front']);
+});
+
+gulp.task('default', [
+  'front'
+]);
