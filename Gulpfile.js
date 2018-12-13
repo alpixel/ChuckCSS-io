8dfc2cc35ca02330251b4b72e88423bf0ebf8777
Mickaël SURREL <mickael@alpixel.fr>
2018-12-13 09:47:09 +0100

    Update documentation for v3.3.1 of ChuckCSS

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,11 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
-var shell = require('gulp-shell');
 var cssnano = require('gulp-cssnano');
 var watch = require('gulp-watch');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var gutil = require('gulp-util');
-var rewriteCSS = require('gulp-rewrite-css');
 var autoprefixer = require('gulp-autoprefixer');
 
 
